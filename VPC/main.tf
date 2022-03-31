a5ff8994acbd42f473a2dad3108159e7c60a5e17
Firuza <fsuyarova@ziyotek.net>
2022-03-30 20:57:37 -0400

    Updated main and variables files

--- VPC/main.tf
+++ VPC/main.tf
@@ -8,7 +8,8 @@ resource "aws_vpc" "main" {
 #Create public subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.pub_subnet_cidr
+availability_zone = "us-east-1a"
 
   tags = {
     Name = "Public Subnet"
@@ -18,8 +19,8 @@ resource "aws_subnet" "public_subnet" {
 #Create private subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-
+  cidr_block = var.prv_subnet_cidr
+availability_zone = "us-east-1b"
   tags = {
     Name = "Private Subnet"
   }
@@ -51,3 +52,26 @@ resource "aws_route_table_association" "a" {
   subnet_id      = "subnet-0ef2820e6a329f45d"
   route_table_id = "rtb-07d63de29755b34b9"
 }
+
+# Creating Private Route Table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "Private Route Table"
+  }
+}
+
+
+# Route Table association with Private Subnet
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+
---
+++ VPC/variable.tf
@@ -0,0 +1,9 @@
+
+
+variable "pub_subnet_cidr"{
+    default = "10.0.3.0/24"
+}
+
+variable "prv_subnet_cidr" {
+  default = "10.0.4.0/24"
+}
